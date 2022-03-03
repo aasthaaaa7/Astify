dc48745ccf0c116ebcc22f01667be278c35b3183
aasthaaaa7 <aasthaaaa7@gmail.com>
2022-03-03 13:57:47 +0530

    Astify

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("welcome to Astify");
+let songs = [
+    {Songname: "Evermore", filepath: "time pass project\1.mp3", coverpath: "Astify - Your favourite music is here_files\cover1.jpg"}
+    {Songname: "Blessings", filepath: "time pass project\2.mp3", coverpath: "Astify - Your favourite music is here_files\cover2.jpg"}
+    {Songname: "Man on the moon", filepath: "time pass project\3.mp3", coverpath: "Astify - Your favourite music is here_files\cover3.jpg"}
+    {Songname: "Dandelions", filepath: "time pass project\4.mp3", coverpath: "Astify - Your favourite music is here_files\cover4.jpg"}
+    {Songname: "Paris", filepath: "time pass project\5.mp3", coverpath: "Astify - Your favourite music is here_files\cover5.jpg"}
+    {Songname: "Young", filepath: "time pass project\6.mp3", coverpath: "Astify - Your favourite music is here_files\cover6.jpg"}
+    {Songname: "Unforgettable", filepath: "time pass project\6.mp3", coverpath: "Astify - Your favourite music is here_files\cover7.jpg"}
+]
+let audioElement = new Audio('1.mp3');
+//audioElement.play();
